4d2525df105d38054edd3d1baec27dbfca590e07
Medi Assumani <mediassumani49@gmail.com>
2018-04-13 11:51:18 -0400

    adding protocol folder

---
+++ TrackHub/CoreData/CoreDataHelper.swift
@@ -0,0 +1,10 @@
+//
+//  CoreDataHelper.swift
+//  TrackHub
+//
+//  Created by Assumani, Medi on 4/13/18.
+//  Copyright © 2018 Assumani, Medi. All rights reserved.
+//
+
+import Foundation
+import CoreData
