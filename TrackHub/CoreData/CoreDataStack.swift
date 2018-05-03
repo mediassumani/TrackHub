708addb80db76fed2eeb91a1ffd7ccd6ba2a81c5
Medi Assumani <mediassumani49@gmail.com>
2018-05-03 13:52:54 -0400

    creating coredata stack

---
+++ TrackHub/CoreData/CoreDataStack.swift
@@ -0,0 +1,10 @@
+//
+//  CoreDataStack.swift
+//  TrackHub
+//
+//  Created by Assumani, Medi on 5/3/18.
+//  Copyright © 2018 Assumani, Medi. All rights reserved.
+//
+
+import Foundation
+import CoreData
