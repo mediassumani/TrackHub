f6bdb567f1ae929f5d3120a81039cfde1d9c62f3
Medi Assumani <mediassumani49@gmail.com>
2018-05-03 13:55:49 -0400

    creating coredata stack

--- TrackHub/CoreData/CoreDataStack.swift
+++ TrackHub/CoreData/CoreDataStack.swift
@@ -8,3 +8,17 @@
 
 import Foundation
 import CoreData
+
+struct CoreDataStack{
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TrackHub")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+        
+}
